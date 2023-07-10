20fd61c42f2715eec351118f52b7e45761ebd3a8
atkzw3 <akiyoshi.t3@gmail.com>
2023-07-11 02:18:30 +0900

    :sparkles: 数値

---
+++ app/section3/numerical_value.py
@@ -0,0 +1,71 @@
+# 数値
+
+num1 = 50 - 5 * 6
+print(num1)
+# 20
+
+num2 = (50 - 5) * 6
+print(num2)
+# 270
+
+num3 = 8 / 5
+print(num3)
+# 1.6
+
+# 割り算の商を整数で取得
+num4 = 8 // 5
+print(num4)
+# 1
+
+# 割り算の余りを取得
+num5 = 8 % 5
+print(num5)
+# 3
+
+num6 = 2 * 3
+print(num6)
+# 6
+
+# べき乗
+num7 = 2 ** 3
+# 2の3乗
+print(num7)
+# 8
+
+# 型変換
+x = 1
+print(x)
+# 1
+
+y = x
+print(y)
+# 1
+
+
+# xの値を変更
+x = 10
+print(x)
+# 10
+
+# yの値は変更されない
+print(y)
+# 1
+
+# round関数
+# 四捨五入
+print(round(3.1415151515151, 2))
+# 3
+
+# mathモジュール
+# 数学関数が色々扱える
+import math
+# 平方根
+result = math.sqrt(25)
+print(result)
+# 5.0
+
+#help関数
+# ヘルプを表示
+help(math)
+# Help on built-in module math:
+print(math)
